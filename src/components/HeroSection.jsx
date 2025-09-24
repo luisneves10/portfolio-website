9f8aec8cffbe47c363a337135d4c1ea838845fb2
luisneves10 <luisneves10@gmail.com>
2025-09-24 17:15:51 +0100

    moved animation on mobile

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,7 @@ export const HeroSection = () => {
 				</div>
 			</div>
 
-			<div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+			<div className="absolute bottom-20 sm:bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
 				<span className="text-sm">Get to know me</span>
 				<ArrowDown className="h-5 w-5 text-primary" />
 			</div>
