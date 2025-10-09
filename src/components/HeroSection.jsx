fdb73d9cae9f5c46da7c794e5af78545846d5a9f
luisneves10 <luisneves10@gmail.com>
2025-10-09 10:08:09 +0100

    deleted qr code
    removed qr code

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,22 +16,12 @@ export const HeroSection = () => {
 					<p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3">
 						a Software Developer from Porto, Portugal.
 					</p>
-					<div className="hidden md:flex opacity-0 animate-fade-in-delay-4 primary-button cursor-pointer">
+					<div className="opacity-0 animate-fade-in-delay-4 primary-button cursor-pointer">
 						<a href="#projects">
 							View My Work
 						</a>
 					</div>
 				</div>
-			</div>
-			<div className="md:hidden flex flex-col items-center gap-3 opacity-0 animate-fade-in-delay-4">
-				<img src="/cv_qrcode.svg" className="w-70 rounded-4xl shadow-md" />
-				<p className="text-xs text-foreground/80 mx-auto opacity-0 animate-fade-in-delay-3">
-					Scan the QR code to get my CV, or click <span className="underline">
-						<a href="/CV_LuisNeves.pdf" target="_blank" rel="noopener noreferrer">
-							here
-						</a>
-					</span>
-				</p>
 			</div>
 
 			<div className="absolute bottom-20 sm:bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
